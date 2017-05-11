a74076f97703c395b0beb2522e62bf9265482a38
kier028 <blicup@ucsc.edu>
2017-05-10 17:17:09 -0700

    Added music and decoding load

--- js/States/play.js
+++ js/States/play.js
@@ -5,14 +5,16 @@ var playState = {
 		console.log('Create: playState');
 		
 		//  Resize our game world to be a 2000 x 2000 square
-		game.world.setBounds(0, 0, 1316, 756);
+		game.world.setBounds(0, 0, 2632,1512);
 		
 		
 		// add image background
 		map = game.add.image(0, 0, 'map1');
+		map.scale.setTo(2, 2);
 		
 		// add player image
-		player = game.add.sprite(0, 0, 'player');
+		player = game.add.sprite(1150, 500, 'player');
+		player.scale.setTo(.4,.4);
 		game.camera.follow(player, Phaser.Camera.FOLLOW_TOPDOWN_TIGHT, 0.1, 0.1);
 
 		player.anchor.setTo(0.5, 0.5);
@@ -30,6 +32,10 @@ var playState = {
 		
 		
 		// Add sprites here:
+
+		// Music and SFX here:
+		music = game.add.audio('music',.8 , true);
+		music.play();
 		
 	},
 	
@@ -46,21 +52,21 @@ var playState = {
 		player.body.velocity.y = 0;
 		
 		if(keyboard.isDown(Phaser.Keyboard.A)){
-			player.body.velocity.x = -300;
+			player.body.velocity.x = -250;
 			//player.animations.play('walkDown');
 		}
 		else if(keyboard.isDown(Phaser.Keyboard.D)){
-			player.body.velocity.x = 300;
+			player.body.velocity.x = 250;
 			//player.animations.play('walkDown');
 		}
 		
 		else if(keyboard.isDown(Phaser.Keyboard.W)){
-			player.body.velocity.y = -300;
+			player.body.velocity.y = -250;
 			player.animations.play('walkUp');
 			
 		}
 		else if(keyboard.isDown(Phaser.Keyboard.S)){
-			player.body.velocity.y = 300;
+			player.body.velocity.y = 250;
 			player.animations.play('walkDown');
 		}
 		else{
