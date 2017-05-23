01980ed0864ca31fecaec85a0265bf587e66be1d
kier028 <blicup@ucsc.edu>
2017-05-23 14:06:22 -0700

    test

--- js/States/load.js
+++ js/States/load.js
@@ -6,23 +6,37 @@ var loadState = {
 		console.log('Preload: loadState');
 		
 		// Adding a loading label
-		var loadingLabel = game.add.text(80,150,'loading...',{font: '30px Courier', fill: '#ffffff'});
+		var loadingLabel = game.add.text(300,275,'loading...',{font: '30px Courier', fill: '#ffffff'});
 		
 		// Images for game
 		game.load.path = 'assets/';
-		game.load.image('map1', 'testmap.png');
 		game.load.atlas('player','img/playerAtlas.png','img/player.json');
-        //game.load.spritesheet('player', 'player.png', 80, 115);
-
+        game.load.tilemap('level','img/map1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('bgTile','img/temp.png');
+        game.load.image('roadTile','img/roads.png');
+        game.load.image('buildingTile','img/buildings.jpeg');
+        game.load.image('pothole','img/pothole.png');
+        game.load.image('car','img/car.png');
+        game.load.image('fill', 'img/box.png');
         // hud 
         game.load.image('timerbox','img/hud/trans.png');
 		game.load.image('bar','img/hud/bar.png');
+		game.load.spritesheet('inventory', 'img/hud/inventory.png', 132, 82);
+
 
 
 		// Sounds for game
-
+		// MUSIC
 		game.load.path = 'assets/sounds/music/';
-		game.load.audio('music', ['sneak.mp3', 'sneak.ogg']);
+		//game.load.audio('music1', ['sneak.mp3', 'sneak.ogg']);
+		game.load.audio('alert', ['alert.mp3', 'alert.ogg']);
+		game.load.audio('alert-theme', ['alert-theme.mp3', 'alert-theme.ogg']);
+		game.load.audio('caution-theme',['caution-theme.mp3', 'caution-theme.ogg']);
+
+		// SFX
+		game.load.path = 'assets/sounds/sfx/';
+		game.load.audio('yay', ['yay.mp3', 'yay.ogg']);
+		
 	},
 	
 	create: function () {
@@ -36,7 +50,10 @@ var loadState = {
 
 	// Checks to see if audio is decoded!
 	update: function() {
-		if (this.cache.isSoundDecoded('music'))	{
+		if (this.cache.isSoundDecoded('alert') 
+			&& this.cache.isSoundDecoded('alert-theme')
+			&& this.cache.isSoundDecoded('caution-theme')
+			&& this.cache.isSoundDecoded('yay')) {
 			game.state.start('menu');
 		}
 	}
--- js/States/play.js
+++ js/States/play.js
@@ -1,21 +1,89 @@
 // Instantiate playState
-var time = 60000 * 2 ; // value is ms
+var car;
+var tween;
+var bmd;
+var points;
+var count = 1;
+var px = [0];
+var py = [0];
+var potCount = 0;
+
 
 var playState = {
 	create: function () {
+		this.dead = false;
+		this.spotted = false;
+		this.ammo = 0;
+
+		//TIME FOR LEVEL
+		this.time = 60000 / 2; 
 		
 		console.log('Create: playState');
 		
 		//  Resize our game world to be a 2000 x 2000 square
 		game.world.setBounds(0, 0, 2632,1512);
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 		
-		
+        bmd=null;
+        
+        //Plot points for the moving sprite(car). The same index in x and y 
+        //array correspond to one point (x[1] = 600, y[1] = 600) is 600, 600 
+        //on coordinate plane
+        points = {
+            'x' : [64, 480,480,64,64],
+            'y' : [480, 480,64,64,480]
+        };
+        
+        //Prelim variable instantiation
+        //Make increment smaller for faster moving sprite and vice versa
+        increment = 1/400;
+        i = 0;
+        timerStopped = true;
+        timer1 = null;
+        
+        //Creating bitmap
+        bmd = this.add.bitmapData(game.width, game.height);
+        bmd.addToWorld();
+        
+        //Loop to draw path for visualization
+        /*for(let j = 0; j < 1; j += increment) {
+            var posx = this.math.linearInterpolation(points.x, j);
+            var posy = this.math.linearInterpolation(points.y, j);
+            bmd.rect(posx, posy, 3, 3, 'rgba(245, 0, 0, 1)');
+        }*/
+        
 		// add image background
-		map = game.add.image(0, 0, 'map1');
-		map.scale.setTo(2, 2);
+        map=game.add.tilemap('level');
+        map.addTilesetImage('temp','bgTile');
+        map.addTilesetImage('roads','roadTile');
+        map.addTilesetImage('buildings','buildingTile');
+        mapBackground = map.createLayer('Background');
+        mapBuildings = map.createLayer('Buildings');
+        mapRoads = map.createLayer('Roads');
+        map.setCollisionBetween(1,3000,true,'Buildings');
+        mapBackground.resizeWorld();
+		//map = game.add.image(0, 0, 'map1');
+		//map.scale.setTo(2, 2);
 		
+        // add pothole image ------> convert to Group!!! 
+        potholes = game.add.group(); 
+     
+        //potholes.create(1000,400,'pothole'); 
+        //createPothole(1000, 300); 
+     
+        pothole = game.add.sprite(481, 400, 'pothole'); 
+        pothole.scale.setTo(.2,.2); 
+        pothole.anchor.setTo(0.5,0.5); 
+     
+        // pothole physics 
+        game.physics.enable(pothole, Phaser.Physics.ARCADE); 
+     
+        // adjusting hitbox size for pothole  
+        pothole.body.setSize(200, 175, 75, 75); 
+        pothole.body.immovable = true; 
+        
 		// add player image
-		player = game.add.sprite(1150, 500, 'player');
+		player = game.add.sprite(500, 500, 'player');
 		player.scale.setTo(.4,.4);
 		game.camera.follow(player, Phaser.Camera.FOLLOW_TOPDOWN_TIGHT, 0.1, 0.1);
 
@@ -25,14 +93,17 @@ var playState = {
         player.animations.add('walkRight',Phaser.Animation.generateFrameNames('playerSide_',6,1,'',2),20,false);
         player.animations.add('walkLeft',Phaser.Animation.generateFrameNames('playerSide_',7,12,'',2),20,false);
         player.animations.play('walkDown');
-        //player.animations.add('walkDown', [0,1,2,3,4,5], 20, true);
-		//player.animations.add('walkUp', [6,7,8,9,10,11], 20, true);
 		
+        //Adding test car
+        car = new Car(game, 'car', 900, 500);
+        game.add.existing(car);
+        //tween = game.add.tween(car).to({x: [600, 900]}, 1000, "Linear", true, -1, false);
+        //tween.onComplete.addOnce(this.tween2, this);
+        
 		// player physics
 		game.physics.enable(player, Phaser.Physics.ARCADE);
 		player.body.collideWorldBounds = true;
 		
-
 			
 		// Add Keyboard movement/actions here:
 		keyboard = game.input.keyboard;
@@ -40,54 +111,121 @@ var playState = {
 		
 		// Add sprites here:
 
-
+		// CEMENT FILLER TEST
+		fill = game.add.sprite(700, 500,'fill');
+		game.physics.enable(fill, Phaser.Physics.ARCADE);
+		fill.body.immovable = true;
 
 
 		// hud  here:
 
-		// TIMER
-		box = game.add.sprite(20, 20, 'timerbox');
-		box.anchor.setTo(.5);
-		box.fixedToCamera = true;
-		box.cameraOffset.setTo(400, 40);
-		box.scale.setTo(1.2,1);
-		timer = game.add.text(20,20, '',
-								{font: '32px Comic Sans MS', fill: '#FFFFFF' });
-		timer.anchor.setTo(.5);
-		timer.fixedToCamera = true;
-		timer.cameraOffset.setTo(400, 35);
-
 		// Admiration Levels
 		bar = game.add.sprite(700, 100, 'bar');
 		bar.anchor.setTo(.5);
-		box.scale.setTo(1.2,1.4);
 		bar.fixedToCamera = true;
 		bar.cameraOffset.setTo(775, 425);
+
+		// Filler Inventory
+		inv = game.add.sprite(0, 20, 'inventory');
+		inv.animations.add('empty', [0], false);
+		//inv.animations.add('1', [1], false);
+		//inv.animations.add('2', [2], false);
+		//inv.scale.setTo(.8,.8)
+		//inv.anchor.setTo(.5);
+		inv.fixedToCamera = true;
+		inv.cameraOffset.setTo(10, 500);
+		inv.animations.play('empty');
 
 
 
 
 		// Music and SFX here:
-		music = game.add.audio('music',.8 , true);
-		music.play();
-		
+		music_caution = game.add.audio('caution-theme',.8 , true);
+		music_alert = game.add.audio('alert-theme', .8, true);
+		sfx_alert = game.add.audio('alert', .9, false);
+		music_caution.play();
 	},
+
+
+
+
+	gameOver: function(){
+		player.kill();
+		this.gameover = game.add.text(400 , game.world.height/2, 'GAMEOVER\nPress "r" to return to menu ',
+										{font: '30px Comic Sans MS', fill: '#FFFFFF'});
+		this.gameover.fixedToCamera = true;
+		this.gameover.anchor.setTo(.5);
+		this.gameover.cameraOffset.setTo(400, 300);
+		this.dead = true;
+	},
+
+	collectFill: function(){
+		box = game.add.sprite(20, 20, 'timerbox');
+		box.scale.setTo(1.2,1.4);
+		box.anchor.setTo(.5);
+		box.fixedToCamera = true;
+		box.cameraOffset.setTo(400, 40);
+		box.scale.setTo(1.2,1);
+		timer = game.add.text(20,20, '',
+				{font: '32px Comic Sans MS', fill: '#FFFFFF' });
+		timer.anchor.setTo(.5);
+		timer.fixedToCamera = true;
+		timer.cameraOffset.setTo(400, 35);
+		sfx_alert.play();
+		music_caution.stop();
+		music_alert.play();
+		this.spotted = true;
+		if(this.ammo<=2) { 
+			this.ammo++;
+			inv.frame = this.ammo;
+			fill.kill();
+		}
+
+	},
+
+
+
+
+
+
 	
 	update: function() {
+
 		
 		
-		console.log('Update: playState');
+		//console.log('Update: playState');
+		if(this.time == 0){
+			this.gameOver();
+		}
 
 		// time:
-		timer.text = '' + Math.max( Math.round(time)/1000, 0.0 ).toFixed(1); ;
-		time = time - 20;
-		
+//		timer.text = '' + Math.max( Math.round(this.time)/1000, 0.0 ).toFixed(1);
+//		this.time = this.time - 20;
+
+		// once player picks up the fill timer starts ticking down;
+
+		if(this.spotted == true){
+			timer.text = '' + Math.max( Math.round(this.time)/1000, 0.0 ).toFixed(1); 
+			this.time = this.time - 20;
+
+		}
 
 		
+        if(timerStopped){
+            timerStopped = false;
+            timer1 = game.time.create(true);
+            timer1.loop(.01, this.plot, this);
+            timer1.start();
+        }
+		
 		// Add collision:
+		game.physics.arcade.overlap(player, fill, this.collectFill, null, this);
+
+		game.physics.arcade.collide( player, mapBuildings);
+        game.physics.arcade.overlap(player,car,this.wasHit,null,this);
+
 		
 		// Add conditions for movement/actions here:
-		
 		player.body.velocity.x = 0;
 		player.body.velocity.y = 0;
 		
@@ -113,8 +251,42 @@ var playState = {
 			//player.animations.stop();
 			player.frame = 'player_01';
 		}
+        
 		
-		
+		// Condition for removing a pothole
+		// (SOLVED)Bug: if the player holds down the space bar it is continually updating the potCount.
+		// This is why it is set to 50. It is a bit inconsistant with the spacebar pressing, but works as intended.
+		// Specify in the "Instructions" that the player is to tap on spacebar.
+		if (game.physics.arcade.overlap(player, pothole) == true && game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)){ 
+            potCount++;
+			if ( potCount == 10 ){
+				pothole.destroy();
+				potCount == 0;
+			}
+        }
+        
+        //pothole.events.onInputDown.add(destroySprite, this); 
+        
+        // debug section~!!!!!~ 
+        //game.debug.bodyInfo(player, 32, 32); 
+ 
+        //game.debug.body(player); 
+        //game.debug.body(car); 
+        //game.debug.body(pothole); 
+
+
+        if(this.dead==true){
+        	if(keyboard.isDown(Phaser.Keyboard.R)){
+        		music_caution.stop();
+        		music_alert.stop();
+
+        		game.state.start('menu');
+
+			}
+
+        }
+     
+        
 	},
 	
 	Win: function() {
@@ -123,5 +295,51 @@ var playState = {
 		
 		// Function call to winState.
 		game.state.start('win');
-	}
+	},
+    wasHit: function(){
+        console.log('wasHit');
+    },
+    plot: function(){
+        var posx = this.math.linearInterpolation(points.x, i);
+        var posy = this.math.linearInterpolation(points.y, i);
+        car.x = posx;
+        car.y = posy;
+        i += increment;
+        
+        px.push(posx);
+        py.push(posy);
+        //console.log(px);
+        
+        angle = this.math.angleBetween(px[count-1], py[count-1], posx, posy);
+        car.rotation = angle;
+        //posy in this case will terminate the sprite when it reaches a certain
+        //y-position. Can be changed to terminate upon reaching certain 
+        //x-pos
+        if(posy > 480) {
+            timer1.stop();
+            timer1.destroy();
+            i = 0;
+            timerStopped = true;
+        }
+        count++;
+    },
+    createPothole: function(x,y){ 
+    potholes.create(x,y, 'pothole'); 
+     
+    //potholes.scale.setTo(.2,.2); 
+    //potholes.anchor.setTo(0.5,0.5); 
+     
+    //pothole physics 
+    //game.physics.enable(potholes, Phaser.Physics.ARCADE); 
+     
+    //adjusting hitbox size  
+    //potholes.body.setSize(200, 175, 75, 75); 
+    //potholes.body.immovable = true; 
+  }, 
+   
+   
+  //function destroySprite(pothole){ 
+     
+  //}
+    
 }
