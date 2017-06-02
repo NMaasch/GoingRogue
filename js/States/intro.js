cf74239114c73bbf1ca63ac71980d070f2f526c0
Unknown <shipnar13@gmail.com>
2017-06-01 22:33:30 -0700

    Added more to intro

--- js/States/intro.js
+++ js/States/intro.js
@@ -6,10 +6,58 @@ var introState = {
     
     create: function(){
         keyboard=game.input.keyboard;
-        introText=game.add.text(400,300,'In A World',{fontSize: 64,fill:'white'});
-        introText.anchor.set(0.5);
-        //fades text out
-        game.add.tween(introText).to({alpha:0},5000,"Linear",true);
+        timer=game.time.create(true);
+        
+        timer.add(1000,function(){
+            introText = game.add.text(400,300,'In A World',{fontSize:64, fill:'white'});
+            introText.anchor.set(0.5);
+            game.add.tween(introText).to({alpha:0},3000,"Linear",true);
+            },this
+        );
+       timer.add(4000,function(){
+            introText = game.add.text(400,300,'Filled with Despair',{fontSize:64, fill:'white'});
+            introText.anchor.set(0.5);
+            game.add.tween(introText).to({alpha:0},3000,"Linear",true);
+            },this
+        );
+        timer.add(7000,function(){
+            introText = game.add.text(400,300,'One Man',{fontSize:64, fill:'white'});
+            introText.anchor.set(0.5);
+            game.add.tween(introText).to({alpha:0},3000,"Linear",true);
+            },this
+        );
+        timer.add(10000,function(){
+            introText = game.add.text(400,300,'Will Save His People',{fontSize:64, fill:'white'});
+            introText.anchor.set(0.5);
+            game.add.tween(introText).to({alpha:0},3000,"Linear",true);
+            },this
+        );
+        timer.add(13000,function(){
+            introText = game.add.text(400,300,'From the Negligent\n\t\t\t\t\t\tGovernment',{fontSize:64, fill:'white'});
+            introText.anchor.set(0.5);
+            game.add.tween(introText).to({alpha:0},3000,"Linear",true);
+            },this
+        );
+        timer.add(16000,function(){
+            introText = game.add.text(400,300,'By',{fontSize:64, fill:'white'});
+            introText.anchor.set(0.5);
+            game.add.tween(introText).to({alpha:0},3000,"Linear",true);
+            },this
+        );
+        timer.add(19000,function(){
+            introText = game.add.text(400,300,'Fixing Potholes',{fontSize:64, fill:'white'});
+            introText.anchor.set(0.5);
+            game.add.tween(introText).to({alpha:0},3000,"Linear",true);
+            },this
+        );
+        timer.add(22000,function(){
+            introText = game.add.text(400,300,'Press [Spacebar] to liberate',{fontSize:56, fill:'white'});
+            introText.anchor.set(0.5);
+            },this
+        );
+        
+        timer.start();
+        
     },
     update: function(){
         if(keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
@@ -17,6 +65,5 @@ var introState = {
         }
         
     },
-    
     
 }
