16c82c87f17885a33eea8d4791fa906d7e9e038a
jbermeo1996 <jennifer.bermeo.c@gmail.com>
2017-06-02 12:18:06 -0700

    added logo

--- js/States/menu.js
+++ js/States/menu.js
@@ -3,6 +3,7 @@ var menuState = {
 	
 	preload: function(){
 		game.load.path = 'assets/';
+		this.load.image('logoName', 'img/logoName.png');
 		this.load.image('startB', 'img/start.png');
     	this.load.image('instructionsB', 'img/Instructions.png');
         this.load.image('bg','img/bng.png');
@@ -10,20 +11,31 @@ var menuState = {
 
 	create: function () {
 		
+		//console log, setting bounds of game, setting camera
 		console.log('Create: menuState');
         game.world.setBounds(0,0,800,600);
-        this.add.image(0,0,'bg');
-        keyboard=game.input.keyboard;
-    	startButton = this.add.button(this.world.centerX - 65, 150,'startB', this.startGame, this);
         game.camera.setPosition(0,0);
-    	startInstuction = this.add.button(this.world.centerX - 65, 300,'instructionsB', this.goToInstructions, this);
+        
+        //setting variable keyboard
+        keyboard=game.input.keyboard;
+
+        //adding background and logo
+        this.add.image(0,0,'bg');
+        this.add.image(this.world.centerX-200,100,'logoName');
+
+        //adding buttons to game + leads to function 
+    	startButton = this.add.button(this.world.centerX - 65, 220,'startB', this.startGame, this);
+    	startInstuction = this.add.button(this.world.centerX - 65, 350,'instructionsB', this.goToInstructions, this);
 	},
+	
+	// can start game by pressing spacebar 
     update: function(){
         if(keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
             this.state.start('play');
         }
     },
 	
+	//fuctions for buttons 
 	 startGame: function() {
      	this.state.start('play');
 	},
